307a3cb374eaf1420cf921f88095ef9b1ebfe4f1
Panny Lewis <pannylewis@gmail.com>
2020-06-13 13:01:50 -0500

    App is parent, Pizza is child, Form is grandchild, Form is passed on to Pizza which in turn is passed on to App which in turn renders to browswer.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import React from "react";
+import "./App.css";
+import Pizza from "./components/Pizza";
+import { Switch, Link, Route } from "react-router-dom";
 
 const App = () => {
   return (
     <>
-      <h1>Lambda Eats</h1>
-      <p>You can remove this code and create your own header</p>
+      <Route exact path="/">
+        <Link to="/pizza">Pizza</Link>
+        <div className="App">
+          <h1>Lambda Eats</h1>
+        </div>
+      </Route>
+      <Route path="/form">
+        <Pizza />
+      </Route>
     </>
   );
 };
